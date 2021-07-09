a8c36cc4697f3b0fe1cec813edaeb3b8b767d6f0
krishmupri <krishmupri@gmail.com>
2021-07-09 20:02:25 +0530

    leetcode 34 anothe approach commit

--- src/FirstAndLastPositionOfElement.java
+++ src/FirstAndLastPositionOfElement.java
@@ -8,6 +8,62 @@ public class FirstAndLastPositionOfElement {
 
         int target = 6;
         int[] a = solve(nums, target);
+        //adding another approach to solve this
+
+        int sol[]=new int[]{-1,-1};
+
+
+        int i=0;
+        int j=nums.length-1;
+
+       sol[0]=index(nums,target,false);
+
+       if(sol[0]!=-1){
+           sol[1]=index(nums,target,true);
+       }
+
+System.out.println(sol[0]+" "+sol[1]);
+
+    }
+
+
+
+
+
+    public static int index(int nums[],int target,boolean x){
+        int keyIndex=-1;
+        int i=0;
+        int j=nums.length-1;
+        while(i<=j){
+
+            int mid=(i+j)/2;
+
+           if(nums[mid]<target)i=mid+1;
+           else if(nums[mid]>target)j=mid-1;
+
+
+
+
+           else{
+               keyIndex=mid;
+               if(x){
+                   i=mid+1;
+               }
+               else j=mid-1;
+
+
+
+
+           }
+
+
+
+        }
+
+
+
+
+        return keyIndex;
     }
     public static int[]  solve(int [] nums,int target){
         int i=0;
